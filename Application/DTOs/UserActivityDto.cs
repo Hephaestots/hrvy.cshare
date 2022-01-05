b3967f256b81c535ec594be1e9063e4c24b03623
MeatyHarvey <harveyvincent1@gmail.com>
2022-01-04 20:47:16 -0500

    Challenge : Profile/{username}/activities with filtering.

---
+++ Application/DTOs/UserActivityDto.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Text.Json.Serialization;
+
+namespace Application.DTOs
+{
+    public class UserActivityDto : BaseEntity<Guid>
+    {
+        public string? Title { get; set; }
+        public string? Category { get; set; }
+        public DateTime Date { get; set; }
+
+        [JsonIgnore]
+        public string? HostUsername { get; set; }
+    }
+}
