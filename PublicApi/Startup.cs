e327e8efa35004f83195abb6239e5b8c5992f997
MeatyHarvey <harveyvincent1@gmail.com>
2022-01-08 22:14:18 -0500

    Security Headers added.

--- PublicApi/Startup.cs
+++ PublicApi/Startup.cs
@@ -66,7 +66,7 @@ namespace API
             {
                 app.Use(async (context, next) =>
                 {
-                    context.Response.Headers.Add("Scrict-Transport-Security", "max-age=31536000");
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
                     await next.Invoke();
                 });
             }
